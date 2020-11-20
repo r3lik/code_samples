38b6888888c5375fcc81e62965a269b875dc1dfb
r3lik <mike@r3lik.com>
2020-11-20 15:43:05 -0800

    Add pointer exercise - details

--- go/greatercommons/pointers/exercises/main.go
+++ go/greatercommons/pointers/exercises/main.go
@@ -16,18 +16,23 @@ func main() {
 	fmt.Println(p1)
 	fmt.Printf("%p\n", &p1)
 	// changes values at same address
-	changeMe(&p1)
+	a1 := changeMe(&p1)
 	fmt.Println(p1)
 	fmt.Printf("%p\n", &p1)
+
+	// print address where values were modified above
+	fmt.Printf("%p\n", a1)
+
 	// creates new address and changes values
 	p2 := newAddress(p1)
 	fmt.Println(p2)
 	fmt.Printf("%p\n", &p2)
 }
 
-func changeMe(p *person) {
+func changeMe(p *person) *person {
 	(*p).name = "Ms Whoever"
 	(*p).gender = "female"
+	return p
 }
 
 func newAddress(p person) person {
